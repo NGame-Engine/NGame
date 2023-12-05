46ddd341f443a48f0dc8a2e943ee87f3ee3f7c3f
Raphael Schmitz <raphaelschmitz00@gmail.com>
2023-12-05 18:09:04 +0100

    Add scene file type

--- Shared/NGame.Shared/Assets/AssetConventions.cs
+++ Shared/NGame.Shared/Assets/AssetConventions.cs
@@ -5,6 +5,7 @@ namespace NGame.Assets;
 public static class AssetConventions
 {
 	public static readonly string AssetIdPropertyName = "Id";
+	public static readonly string SceneFileEnding = ".ngscene";
 	public static readonly string AssetFileEnding = ".ngasset";
 	public static readonly string AssetPackSubFolder = ".assets";
 	public static readonly string TableOfContentsFileName = "toc.json";
