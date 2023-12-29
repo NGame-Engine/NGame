8178fabc039a8c2a354f426fc0642eca13ffb891
Raphael Schmitz <raphaelschmitz00@gmail.com>
2023-12-29 22:03:44 +0100

    Try to ignore warning

--- Shared/NGame.Shared/Ecs/IEntityEditor.cs
+++ Shared/NGame.Shared/Ecs/IEntityEditor.cs
@@ -5,6 +5,7 @@ namespace NGame.Ecs;
 public interface IEntityEditor
 {
 	event Action<Entity> EntityCreated;
+	// ReSharper disable once EventNeverInvoked.Global
 	event Action<Entity> EntityRemoving;
 	event Action<EntityComponent> ComponentAdded;
 	event Action<EntityComponent> ComponentRemoving;
