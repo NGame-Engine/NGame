68cbc18e0fcaecefb753d25361a96054153d5879
Raphael Schmitz <raphaelschmitz00@gmail.com>
2023-12-31 11:13:49 +0100

    Add design data for project history view that doesn't work yet

---
+++ Editor/NGameEditor.Avalonia/LauncherWindows/ProjectOperationsViews/ProjectOperationsDesignData.cs
@@ -0,0 +1,10 @@
+﻿using NGameEditor.ViewModels.LauncherWindows;
+
+namespace NGameEditor.Avalonia.LauncherWindows.ProjectOperationsViews;
+
+
+
+public static class ProjectOperationsDesignData
+{
+	public static ProjectOperationsViewModel ProjectOperationsExample { get; } = new();
+}
