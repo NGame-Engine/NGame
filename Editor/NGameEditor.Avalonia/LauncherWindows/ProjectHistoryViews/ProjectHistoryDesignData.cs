958784f2df11d759d5132598a7b5b0866384926a
Raphael Schmitz <raphaelschmitz00@gmail.com>
2023-12-31 11:08:19 +0100

    Add design data for project history view

---
+++ Editor/NGameEditor.Avalonia/LauncherWindows/ProjectHistoryViews/ProjectHistoryDesignData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NGameEditor.ViewModels.LauncherWindows.HistoryViews;
+
+namespace NGameEditor.Avalonia.LauncherWindows.ProjectHistoryViews;
+
+
+
+public static class ProjectHistoryDesignData
+{
+	public static ProjectHistoryViewModel ProjectHistoryExample { get; } =
+		new()
+		{
+			ProjectUsages =
+			{
+				new HistoryEntryViewModel(
+					"/mypath/FirstProject.sln",
+					DateTime.UnixEpoch,
+					ReactiveCommand.Create(() => { })
+				),
+
+				new HistoryEntryViewModel(
+					"/another path/SecondProject.sln",
+					DateTime.UnixEpoch,
+					ReactiveCommand.Create(() => { })
+				),
+
+				new HistoryEntryViewModel(
+					"/a third parth/ThirdProject.sln",
+					DateTime.UnixEpoch,
+					ReactiveCommand.Create(() => { })
+				),
+			}
+		};
+}
