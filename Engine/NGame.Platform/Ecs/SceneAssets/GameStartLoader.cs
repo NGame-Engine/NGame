9e8d5c2c887fecbc8f98dd4d2f5968ce8f68612b
Raphael Schmitz <raphaelschmitz00@gmail.com>
2024-03-04 00:03:34 +0100

    Add appsettings.json loading on android

--- Engine/NGame.Platform/Ecs/SceneAssets/GameStartLoader.cs
+++ Engine/NGame.Platform/Ecs/SceneAssets/GameStartLoader.cs
@@ -15,7 +15,6 @@ public class GameStartLoader(
 	public void OnBeforeApplicationStart()
 	{
 		var startSceneId = validSceneAssetsConfiguration.StartSceneId;
-		startSceneId = Guid.Parse("0F85A235-5A85-4BFB-8BCB-2B7CAF7BF8CC"); // TODO Get appsettings.json on android
 		var scene = sceneLoader.Load(startSceneId);
 		rootSceneAccessor.SetRootScene(scene);
 	}
